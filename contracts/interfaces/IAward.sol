0ffb0590127fff31211a0b1ed401b9d1f5470ed7
MER Tech <service@detf.fi>
2020-11-18 01:02:07 +0800

    remove warnings

--- contracts/interfaces/IAward.sol
+++ contracts/interfaces/IAward.sol
@@ -1,12 +1,9 @@
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.6.12;
 
 interface IAward {
-
     function addFreeAward(address _user, uint256 _amount) external;
-
     function addAward(address _user, uint256 _amount) external;
-
     function withdraw(uint256 _amount) external;
-
     function destroy(uint256 amount) external;
 }
